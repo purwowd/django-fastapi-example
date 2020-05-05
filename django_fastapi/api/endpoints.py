d586198b15aa3d7a39fcf31d8c86aa0d1049175b
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-05-04 18:50:20 -0700

    A better way to authenticate users in the POST request
    Can we just use @login_required?

--- django_fastapi/api/endpoints.py
+++ django_fastapi/api/endpoints.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Response, status, Depends
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 from api import models, schemas
 
@@ -14,19 +15,14 @@ api_router = APIRouter()
 @api_router.post("/items", response_model=schemas.Item)
 def create_item(item: schemas.ItemCreate, response: Response, credentials: HTTPBasicCredentials = Depends(security)):
 
-    try:
-        user = User.objects.get(username=credentials.username)  # get the user object - if it doesn't exist, the exception block throws unauthorized
-        if not user.check_password(credentials.password):  # check the password - if it's wrong, throw 401 unauthorized
-            response.status_code = status.HTTP_401_UNAUTHORIZED
-            return None
-    except User.DoesNotExist:
+    user = authenticate(username=credentials.username, password=credentials.password)
+    if user is not None:
+        item = models.Item.objects.create(**item.dict(), owner_id=user.id)
+        response.status_code = status.HTTP_201_CREATED
+        return item
+    else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return None
-
-    item = models.Item.objects.create(**item.dict(), owner_id=user.id)
-    response.status_code = status.HTTP_201_CREATED
-
-    return item
 
 
 @api_router.get("/items", response_model=List[schemas.Item])
