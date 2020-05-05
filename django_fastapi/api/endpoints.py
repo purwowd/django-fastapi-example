2ac2d6962d58239fc4f8d7481647a4539c78cc41
Nick Santos <ns-ffc_api_client@maskedmail.net>
2020-05-04 18:05:45 -0700

    Adding basic POST security - would be good to make this a decorator that checks a permission on the user
    Read about best way to do this with FastAPI

--- django_fastapi/api/endpoints.py
+++ django_fastapi/api/endpoints.py
@@ -1,16 +1,30 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from django.contrib.auth.models import User
 
 from api import models, schemas
 
-
+security = HTTPBasic()
 api_router = APIRouter()
 
 
 @api_router.post("/items", response_model=schemas.Item)
-def create_item(item: schemas.ItemCreate):
-    item = models.Item.objects.create(**item.dict())
+def create_item(item: schemas.ItemCreate, response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+
+    try:
+        user = User.objects.get(username=credentials.username)  # get the user object - if it doesn't exist, the exception block throws unauthorized
+        if not user.check_password(credentials.password):  # check the password - if it's wrong, throw 401 unauthorized
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            return None
+    except User.DoesNotExist:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return None
+
+    item = models.Item.objects.create(**item.dict(), owner_id=user.id)
+    response.status_code = status.HTTP_201_CREATED
 
     return item
 
